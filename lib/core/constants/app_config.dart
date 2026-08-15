c5ced31d473e6d80354dcdb7c353a4e7adbce587
muzammil608 <muzmalabbas579@gmail.com>
2026-08-16 00:16:54 +0500

    feat(release): ShopFlow POS v1.2.0-beta.5 clean build

--- lib/core/constants/app_config.dart
+++ lib/core/constants/app_config.dart
@@ -18,8 +18,8 @@ class AppConfig {
   static const String appName = 'ShopFlow POS';
   
   /// Application version string (synchronize with pubspec.yaml and windows/installer.iss)
-  static const String currentVersion = '1.2.0-beta.4';
-  static const String buildSignature = '1.2.0-beta.4+66';
+  static const String currentVersion = '1.2.0-beta.5';
+  static const String buildSignature = '1.2.0-beta.5+67';
 
   /// Active update channel (defaults to beta channel for development builds)
   static UpdateChannel defaultChannel = UpdateChannel.beta;
