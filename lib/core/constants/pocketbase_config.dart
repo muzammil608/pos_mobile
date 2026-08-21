c9f1d9d9d3256f28e755e6fa17213c5c3b715466
muzammil608 <muzmalabbas579@gmail.com>
2026-08-21 21:59:32 +0500

    feat(release): ShopFlow POS v1.2.0-beta.23 fresh release

--- lib/core/constants/pocketbase_config.dart
+++ lib/core/constants/pocketbase_config.dart
@@ -12,5 +12,5 @@ class PocketBaseConfig {
   static const String superuserPassword = 'REMOVED';
 
   /// Unique build signature to ensure fresh installations land on the Login Screen.
-  static const String buildSignature = '1.2.0-beta.22+22';
+  static const String buildSignature = '1.2.0-beta.23+23';
 }
